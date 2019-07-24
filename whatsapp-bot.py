fbb8b61d3ed331b345a68b76b3fa4cf000e06aec
Tarun Bansal <tarun.bansal6199@gmail.com>
2019-07-25 01:53:30 +0530

    Update whatsapp-bot.py

--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -3,19 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
- 
 import time
 import sys
  
-# Replace below path with the absolute path
-# to chromedriver in your computer
+# Replace below path with the absolute path to chromedriver in your computer
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
  
-# Replace 'Friend's Name' with the name of your friend 
-# or the name of a group 
-target = '"Uncle"'
+# Replace 'Friend's Name' with the name of your friend or the name of a group 
+target = '"Akshita"'
 # Replace the below string with your own message
 string = "This is Virus!"
 x_arg = '//span[contains(@title,' + target + ')]'
